c744e124a45b2dfe161359262377ba492fe09aaf
Dean Abner Julian <dabnerjulian@yahoo.com>
2024-12-06 13:42:15 +0700

    Add new views for Enertek and Penta Sarana with corresponding routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('enertek', function () {
+    return view('enertek');
+});
+
+Route::get('penta-sarana', function () {
+    return view('penta-sarana');
+});
